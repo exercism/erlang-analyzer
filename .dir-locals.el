7c7aeb4e7d3564fc482b4ffbdf3f62d7688939f9
Norbert Melzer <timmelzer@gmail.com>
2019-04-03 13:21:07 +0200

    erlang indent via .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((erlang-mode
+  (erlang-indent-level . 2)))
--- test/export_all_test.erl
+++ test/export_all_test.erl
@@ -7,8 +7,4 @@ does_not_allow_export_all_test() ->
     "-compile(export_all).\n"
     "foo() -> undefined.\n",
   {ok, #{export_all := ExportAll}} = erlang_analyzer:analyze("example/export_all_example.erl", Code),
-  ?assertEqual(ExportAll, true).
-
-%% Local Variables:
-%% erlang-indent-level: 2
-%% End:
+  ?assertNotEqual(#{}, ExportAll).
