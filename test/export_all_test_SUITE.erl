c308d667c553ec5d16fb87f934e5222e2b179c29
Norbert Melzer <timmelzer@gmail.com>
2019-04-06 23:49:15 +0200

    use CT-macro

--- test/export_all_test_SUITE.erl
+++ test/export_all_test_SUITE.erl
@@ -13,13 +13,13 @@ init_per_testcase(does_not_allow_export_all, Config) -> [{file_name, "export_all
 init_per_testcase(does_allow_regular,        Config) -> [{file_name, "export_all_example_good.erl"}|Config].
 
 does_not_allow_export_all(Config) ->
-  {data_dir,  Dir}  = lists:keyfind(data_dir,  1, Config),
-  {file_name, File} = lists:keyfind(file_name, 1, Config),
+  Dir  = ?config(data_dir,  Config),
+  File = ?config(file_name, Config),
   {ok, #{export_all := [{export_all, 2}]}} =
     erlang_analyzer:analyze(Dir, File).
 
 does_allow_regular(Config) ->
-  {data_dir,  Dir}  = lists:keyfind(data_dir,  1, Config),
-  {file_name, File} = lists:keyfind(file_name, 1, Config),
+  Dir  = ?config(data_dir,  Config),
+  File = ?config(file_name, Config),
   {ok, #{export_all := []}} =
     erlang_analyzer:analyze(Dir, File).
