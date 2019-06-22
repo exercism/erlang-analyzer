55e262f74671a94102b8ef17645c3538c38041c5
Norbert Melzer <timmelzer@gmail.com>
2019-06-22 09:36:48 +0200

    create IS_NAME guard macro

--- src/ea_files.erl
+++ src/ea_files.erl
@@ -1,16 +1,20 @@
 -module(ea_files).
 
 -export([filename/2, parse_tree/2]).
--export_type([file/0, tree_node/0]).
+-export_type([file/0, name/0, tree_node/0]).
 
 %% TODO: fix type of `tree` field once https://github.com/inaka/katana-code/issues/27 got fixed.
 -type tree_node() :: map().
 
+-type name() :: string() | binary().
+
 -type file() :: string()
               | binary()
-              | #{ name    := string() | binary()
+              | #{ name    := name()
                  , content => binary()
                  , tree    => tree_node()}.
+
+-define(IS_NAME(Name), (is_binary(Name) orelse is_list(Name))).
 
 %% TODO: use proper type where we have term now
 -spec parse_tree(ea_config:config(), file()) -> {tree_node(), file()}.
@@ -25,6 +29,6 @@ parse_tree(Config = #{project_path := Base}, File) when is_list(File); is_binary
   {ok, Content} = file:read_file(FullPath),
   parse_tree(Config, #{name => File, content => Content}).
 
--spec filename(ea_config:config(), file()) -> string() | binary().
-filename(_Config, File) when is_binary(File); is_list(File) -> File;
+-spec filename(ea_config:config(), file()) -> name().
+filename(_Config, File) when ?IS_NAME(File) -> File;
 filename(_Config, #{name := Name}) -> Name.
