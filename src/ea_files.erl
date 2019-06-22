0df12a1959ff547833b728c2ab190ee450b8b9c7
Norbert Melzer <timmelzer@gmail.com>
2019-06-22 09:45:31 +0200

    introduce full_path/2

--- src/ea_files.erl
+++ src/ea_files.erl
@@ -1,6 +1,6 @@
 -module(ea_files).
 
--export([filename/2, parse_tree/2]).
+-export([filename/2, full_path/2, parse_tree/2]).
 -export_type([file/0, name/0, tree_node/0]).
 
 %% TODO: fix type of `tree` field once https://github.com/inaka/katana-code/issues/27 got fixed.
@@ -24,10 +24,14 @@ parse_tree(Config, File = #{content := Content}) ->
   Tree  = ktn_code:parse_tree(Content),
   File1 = maps:put(tree, Tree, File),
   parse_tree(Config, File1);
-parse_tree(Config = #{project_path := Base}, File) when is_list(File); is_binary(File) ->
-  FullPath = filename:join(Base, File),
+parse_tree(Config, File) ->
+  FullPath = full_path(Config, File),
   {ok, Content} = file:read_file(FullPath),
   parse_tree(Config, #{name => File, content => Content}).
+
+-spec full_path(ea_config:config(), file()) -> file:filename_all().
+full_path(Config = #{project_path := Base}, File) ->
+  filename:join(Base, filename(Config, File)).
 
 -spec filename(ea_config:config(), file()) -> name().
 filename(_Config, File) when ?IS_NAME(File) -> File;
