91adac11bacd8d59c601e51816d708336369a7b8
Norbert Melzer <timmelzer@gmail.com>
2019-06-26 12:28:56 +0200

    add negative tests for each rule in common

--- test/common_rules_test.erl
+++ test/common_rules_test.erl
@@ -32,3 +32,26 @@ positive_gen([{Name, Project, FileName, {Rule, Opts}}|T]) ->
       ea_common_rules:Rule(Config, File, Opts)
     )}|positive_gen(T)]
   end}.
+
+negative_test_() ->
+  Tests = [
+    {no_export_all,        #{}, [{["common-export-all",            "1"], ["export_selective.erl"]},
+                                 {["common-no-test-version",       "0"], ["test_version.erl"]},
+                                 {["common-used-ignored-variable", "0"], ["use_ignored.erl"]}]},
+    {no_test_version,      #{}, [{["common-export-all",            "1"], ["export_selective.erl"]},
+                                 {["common-export-all",            "0"], ["export_all.erl"]},
+                                 {["common-used-ignored-variable", "0"], ["use_ignored.erl"]}]},
+    {use_ignored_variable, #{}, [{["common-export-all",            "1"], ["export_selective.erl"]},
+                                 {["common-export-all",            "0"], ["export_all.erl"]},
+                                 {["common-no-test-version",       "0"], ["test_version.erl"]}]}
+  ],
+  negative_gen(Tests).
+
+negative_gen([]) -> {generator, fun () -> [] end};
+negative_gen([{_Rule, _Opts, []} | Tail]) -> negative_gen(Tail);
+negative_gen([{Rule, Opts, [{Project, FileName}|Other]}|Tail]) ->
+  {generator, fun () ->
+    Config = #{project_path => filename:join([code:priv_dir(erlang_analyzer) | Project])},
+    File   = filename:join(["src"|FileName]),
+    [?_assertMatch([], ea_common_rules:Rule(Config, File, Opts))|negative_gen([{Rule, Opts, Other}|Tail])]
+  end}.
