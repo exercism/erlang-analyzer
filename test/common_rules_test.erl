0450e44a6c1611bf4342018a70ed70b27ada8de2
Norbert Melzer <nmelzer@nobbz.dev>
2019-06-27 08:43:56 +0200

    generate names for common tests

--- test/common_rules_test.erl
+++ test/common_rules_test.erl
@@ -2,17 +2,11 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-reports_no_false_positive_when_there_is_no_export_all_test() ->
-  Config = #{project_path => filename:join([code:priv_dir(erlang_analyzer), "common-export-all", "1"])},
-  File = filename:join("src", "export_selective.erl"),
-  ?assertMatch([], ea_common_rules:no_export_all(Config, File, #{})).
-
-
 positive_test_() ->
   Tests = [
-    {          ["common-export-all",            "0"], ["export_all.erl"],   {no_export_all,        #{}}},
-    {          ["common-no-test-version",       "0"], ["test_version.erl"], {no_test_version,      #{}}},
-    {"simple", ["common-used-ignored-variable", "0"], ["use_ignored.erl"],  {use_ignored_variable, #{}}}
+    {["common-export-all",            "0"], ["export_all.erl"],   {no_export_all,        #{}}},
+    {["common-no-test-version",       "0"], ["test_version.erl"], {no_test_version,      #{}}},
+    {["common-used-ignored-variable", "0"], ["use_ignored.erl"],  {use_ignored_variable, #{}}}
   ],
   positive_gen(Tests).
 
@@ -22,12 +16,8 @@ positive_gen([{Name, Project, FileName, {Rule, Opts}}|T]) ->
   {generator, fun () ->
     Config    = #{project_path => filename:join([code:priv_dir(erlang_analyzer) | Project])},
     File      = filename:join(["src"|FileName]),
-    TestName0 = atom_to_list(Rule) ++ " positive",
-    TestName1 = case Name of
-      "" -> TestName0;
-      _  -> lists:flatten([TestName0, " <", Name, ">"])
-    end,
-    [{TestName1, ?_assertMatch(
+    [{lists:flatten(io_lib:format("~s does trigger ~p", [filename:join(Project ++ ["src"|FileName]), Rule])),
+    ?_assertMatch(
       [#{rule := {ea_common_rules, Rule}}],
       ea_common_rules:Rule(Config, File, Opts)
     )}|positive_gen(T)]
@@ -53,5 +43,6 @@ negative_gen([{Rule, Opts, [{Project, FileName}|Other]}|Tail]) ->
   {generator, fun () ->
     Config = #{project_path => filename:join([code:priv_dir(erlang_analyzer) | Project])},
     File   = filename:join(["src"|FileName]),
-    [?_assertMatch([], ea_common_rules:Rule(Config, File, Opts))|negative_gen([{Rule, Opts, Other}|Tail])]
+    [{lists:flatten(io_lib:format("~s does not trigger ~p", [filename:join(Project ++ ["src"|FileName]), Rule])),
+      ?_assertMatch([], ea_common_rules:Rule(Config, File, Opts))}|negative_gen([{Rule, Opts, Other}|Tail])]
   end}.
